5fcfdbf8395d8478d57a3393836ec3d8ef66607a
Akinselure Abiola <akinselureabiola@Akinselures-MBP.fritz.box>
2023-12-11 08:37:24 +0100

    modifying postgres rds instance

--- terraform/staging/rds.tf
+++ terraform/staging/rds.tf
@@ -6,6 +6,20 @@ resource "random_password" "password" {
 resource "aws_ssm_parameter" "db_password" {
   name  = "/rds/password"
   type  = "String"
-  overwrite = true
   value = random_password.password.result
+}
+
+module "postgres_db" {
+    source = "../modules/rds/"
+
+    db_name                 = "main"
+    identifier              = "postgres"
+    allocated_storage       = "20"
+    engine                   = "postgres"
+    engine_version           = "14"
+    instance_class           = "db.t3.micro"
+    username                 = "staging_db"
+    password                 = aws_ssm_parameter.db_password.value
+    skip_final_snapshot      = var.skip_final_snapshot
+    port                     = "5432"
 }
